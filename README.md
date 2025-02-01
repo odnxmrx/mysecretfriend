b3f6240ba23d25610da8b59c00ba61d2c478e1bc
ArmandoG <armandopinedag@outlook.com>
2025-02-01 10:59:13 -0600

    add functions and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Amigo Secreto - Challenge
+
+Bienvenidos a esta divertida aplicación donde el usuario podrá ingresar y registrar varios nombres de amigos, y después podrá realizar un sorteo para elegir de manera aleatorea a uno de ellos.
+
+## Aplicación
+
+![app running screenshot 1](/assets/amigo-secreto-ss1.png)
+![app running screenshot 2](/assets/amigo-secreto-ss2.png)
--- app.js
+++ app.js
@@ -1,23 +1,58 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-let amigoInput = document.getElementById("amigo");
+const amigoInput = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
 
 function agregarAmigo() {
-  console.log(amigoInput.value);
-  if (amigoInput.value === "") {
+  const valorCadena = amigoInput.value.toLowerCase();
+
+  if (valorCadena === "") {
     alert("Ingre el nombre de un amigo para continuar...");
     return;
   }
-  //   if(!validaTexto(amigoInput.value))
-  //   {
-  //   }
-  amigos.push(amigoInput.value);
+  if (!validaTexto(amigoInput.value)) {
+    alert("Ingresa un nombre válido para continuar...");
+    limpiarYEnfocar();
+    return;
+  }
+
+  amigos.push(valorCadena);
   console.log(amigos);
+
+  // Desplegar lista
+  listarMisAmigos();
+
+  limpiarYEnfocar();
+}
+
+function validaTexto(txt) {
+  const cosita = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$/.test(txt) ? true : false;
+  console.log("cosita: ", cosita);
+  return cosita;
+  //   amigoInput.value = ""; // reseta valor
+  //   limpiarYEnfocar();
+}
+
+function listarMisAmigos() {
+  // Limpiar lista para evitar duplicados
+  listaAmigos.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    listaAmigos.innerHTML += `<ul>${amigos[i]}</ul>`;
+  }
+}
+
+// Funcion para obtener amigo sorteado
+function sortearAmigo() {
+  if (amigos.length === 0) return;
+
+  const indiceATomar = Math.floor(Math.random() * amigos.length);
+  resultado.innerHTML = `<li>${amigos[indiceATomar]}</li>`;
+}
+
+function limpiarYEnfocar() {
   amigoInput.value = ""; // reseta valor
   amigoInput.focus();
 }
-
-// function validaTexto(txt) {
-//   if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$/.test(txt)) return false;
-// }
